972180a805d2e2a3d324d6c566db655c5fbfb8a9
Caleb Grant <geocoug@gmail.com>
2025-08-28 11:29:45 -0700

    Bump version: 0.0.3 → 0.0.4

--- calve/__init__.py
+++ calve/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Caleb Grant"
 __email__ = "grantcaleb22@gmail.com"
 __description__ = "Data management and processing tools for Iceberg tables."
